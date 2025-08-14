af4c53e13ddb1933fa3b84d9a5a1f4ca5b0b4cc3
peterphenikaa <23010678@st.phenikaa-uni.edu.vn>
2025-08-15 02:29:55 +0700

    ket noi database

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 
+const database = require("./config/database.js")
+
 const route = require("./routers/client/index-routes.js");
+
+database.connect()
 
 const app = express();
 const port = process.env.PORT;
@@ -12,6 +16,8 @@ app.set('view engine', 'pug')
 app.use(express.static('public'))
 
 route(app);
+
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
