54627c285f92b13ab00c00e32705efbc49edf90e
peterphenikaa <23010678@st.phenikaa-uni.edu.vn>
2025-08-15 18:08:57 +0700

    trang san pham

--- controllers/client/product-controller.js
+++ controllers/client/product-controller.js
@@ -1,3 +1,21 @@
-module.exports.index = (req, res) => {
-    res.render("client/pages/products/index", {pageTitle: "Trang sản phẩm"});
+const Product = require("../../models/product-model.js")
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    })
+    
+    const newProducts = products.map(item => {
+       item.priceNew = (item.price * (100 - item.discountPercentage) 
+       / 100).toFixed(0)
+       return item
+    })
+
+    console.log(products)
+
+    res.render("client/pages/products/index", 
+    {
+        pageTitle: "Trang sản phẩm",
+        products: newProducts
+    });
 }
