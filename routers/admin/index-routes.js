fafae522f3a3b821648fe82e769eb71a1103e722
peterphenikaa <23010678@st.phenikaa-uni.edu.vn>
2025-08-16 15:35:52 +0700

    layout khung

--- routers/admin/index-routes.js
+++ routers/admin/index-routes.js
@@ -1,10 +1,11 @@
 const dashboardRoutes = require("./dashboard-routes");
+const productRoutes = require("./product-routes");
 const systemConfig = require("../../config/system")
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin
 
   app.use(PATH_ADMIN + "/dashboard", dashboardRoutes);
-  
+  app.use(PATH_ADMIN + "/products", productRoutes);
 };
 
