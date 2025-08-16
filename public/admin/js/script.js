eaf92ca3d72eda6f35d0032f630a1523a658f564
peterphenikaa <23010678@st.phenikaa-uni.edu.vn>
2025-08-16 22:54:38 +0700

    tinh nang tim kiem

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -20,7 +20,25 @@ if (buttonStatus.length > 0 ) {
     })
 }
 
-
+// Form Search
+const formSearch = document.querySelector("#form-search");
+if(formSearch) {
+    let url = new URL(window.location.href)
+    formSearch.addEventListener("submit", (e) => { 
+        // e là Event object - đối tượng chứa thông tin về sự kiện "submit" vừa xảy ra. 
+        
+        e.preventDefault(); // ngăn chặn reload lại trang mặc định
+        
+        const keyword = e.target.elements.keyword.value // là cái value nhận được sau khi bấm submit form
+        
+        if(keyword) {
+            url.searchParams.set("keyword", keyword)
+         } else {
+            url.searchParams.delete("keyword")
+         }
+        window.location.href = url.href
+    });
+}
 
 
 
