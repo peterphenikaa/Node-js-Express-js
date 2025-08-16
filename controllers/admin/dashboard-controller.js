d8ccba0ab4c7dc2e1f38de72ce2c52a29bf7d886
peterphenikaa <23010678@st.phenikaa-uni.edu.vn>
2025-08-16 16:24:18 +0700

    lay data ra san pham

--- controllers/admin/dashboard-controller.js
+++ controllers/admin/dashboard-controller.js
@@ -1,6 +1,9 @@
+// [GET] /admin/dashboard
+
 module.exports.dashboard = (req, res) => {
     res.render("admin/pages/dashboard/index", {
         pageTitle: "Trang tổng quan"
         
     })
 }
+
--- controllers/admin/product-controller.js
+++ controllers/admin/product-controller.js
@@ -1,6 +1,13 @@
-module.exports.index = (req, res) => {
+// [GET] /admin/products
+const Product = require("../../models/product-model.js")
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    })
+    console.log(products)
     res.render("admin/pages/products/index.pug", {
-        pageTitle: "Trang danh sách sản phẩm"
-        
+        pageTitle: "Trang danh sách sản phẩm",
+        products: products
     })
 }
