20cc3bf32904291f680c1849a99ecaa4db67378a
peterphenikaa <23010678@st.phenikaa-uni.edu.vn>
2025-08-20 02:02:52 +0700

    pagination phan 2

--- controllers/admin/product-controller.js
+++ controllers/admin/product-controller.js
@@ -2,6 +2,7 @@
 const Product = require("../../models/product-model.js")
 const filterStatusHelper = require("../../helpers/filterStatus.js")
 const searchHelper = require("../../helpers/search.js")
+const paginationHelper = require("../../helpers/pagination.js")
 
 module.exports.index = async (req, res) => {
     
@@ -19,31 +20,30 @@ module.exports.index = async (req, res) => {
     if (objectSearch.regex) {
        find.title = objectSearch.regex
     }
-
-    // pagination
-    let objectPagination = {
-      currentPage: 1,
-      limitItems: 4
-    }
-
-    if(req.query.page) {
-      objectPagination.currentPage = parseInt(req.query.page)
-    }
     
-    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems
-
+    // Pagination
     const countProducts = await Product.countDocuments(find)
-    const totalPage = Math.ceil(countProducts / objectPagination.limitItems)  // hàm toán làm tròn 4.75 thành 5
-    objectPagination.totalPage = totalPage
+    let objectPagination = await paginationHelper(
+      {
+        currentPage: 1,
+        limitItems: 4
+      },
+      req.query,
+      countProducts
+    )
     
+    // End Pagination
     
-    const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip)
+    const products = await Product.find(find).limit
+    (objectPagination.limitItems).skip(objectPagination.
+    skip);
     
     res.render("admin/pages/products/index.pug", {
-        pageTitle: "Trang danh sách sản phẩm",
-        products: products,
-        filterStatus: filterStatus,
-        keyword: objectSearch.keyword,
-        pagination: objectPagination
+      pageTitle: "Trang danh sách sản phẩm",
+      products: products,
+      filterStatus: filterStatus,
+      keyword: objectSearch.keyword,
+      pagination: objectPagination
     })
 }
+
