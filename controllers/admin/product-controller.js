1ad8f0fd08a4536e9a48217703b70b2a03745ceb
peterphenikaa <23010678@st.phenikaa-uni.edu.vn>
2025-08-23 00:08:06 +0700

    xóa mềm

--- controllers/admin/product-controller.js
+++ controllers/admin/product-controller.js
@@ -87,7 +87,11 @@ module.exports.changeMulti = async (req, res) => {
 module.exports.deleteItem = async (req, res) => {
   const id = req.params.id
 
-  await Product.deleteOne({ _id: id })
+  // await Product.deleteOne({ _id: id })
+  await Product.updateOne({ _id: id }, {
+    deleted: true,
+    deletedAt: new Date()
+  })
 
   const currentUrl = new URL(req.headers.referer || "/admin/products")
   res.redirect(currentUrl.pathname + currentUrl.search)
